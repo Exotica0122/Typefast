6313181af0f7e14566cbddd6409df427843a08aa
Peter An <75343007+Exotica0122@users.noreply.github.com>
2024-04-01 19:40:37 +1300

    refactor: use wordsdisplay

--- src/components/GamePlayWindow.tsx
+++ src/components/GamePlayWindow.tsx
@@ -2,6 +2,7 @@ import { MutableRefObject } from "react";
 import { Caret } from "./Caret";
 import { RandomWord } from "../utils/utils";
 import { Timer } from "./Timer";
+import { WordsDisplay } from "./WordsDisplay";
 
 type GamePlayWindowProps = {
   caretElementPosition: { x: number; y: number };
@@ -29,48 +30,12 @@ export const GamePlayWindow = ({
       <Timer isStarted={isStarted} seconds={seconds} />
       <div className="overflow-hidden h-40">
         <Caret caretElementPosition={caretElementPosition} ref={caretRef} />
-
-        <div
-          className="flex flex-wrap gap-2 text-2xl select-none transition-transform"
-          style={{
-            transform: `translateY(${mainTextTranslateDistance}px)`,
-          }}
-          onClick={() => inputRef.current?.focus()}
-        >
-          {wordsObject.map((wordObject, i) => {
-            return (
-              <div
-                key={wordObject.id}
-                className={`w-fit${
-                  wordObject.isError ? " underline decoration-red-500" : ""
-                }`}
-                ref={(el) => {
-                  if (!el || !wordsRef || !wordsRef.current) return;
-                  wordsRef.current[i] = el;
-                }}
-              >
-                {wordObject.word.map(({ id, value, isError, isTyped }) => {
-                  let letterClassName = "";
-                  if (isTyped && isError) {
-                    letterClassName = "text-red-800";
-                  } else if (isTyped && !isError) {
-                    letterClassName = "text-white";
-                  } else {
-                    letterClassName = "text-neutral-400";
-                  }
-                  return (
-                    <span
-                      key={`${wordObject.id}-${id}`}
-                      className={`${letterClassName}`}
-                    >
-                      {value}
-                    </span>
-                  );
-                })}
-              </div>
-            );
-          })}
-        </div>
+        <WordsDisplay
+          wordsRef={wordsRef}
+          inputRef={inputRef}
+          wordsObject={wordsObject}
+          mainTextTranslateDistance={mainTextTranslateDistance}
+        />
       </div>
     </div>
   );
