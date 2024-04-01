166af828423645c5ce013535ea702c9ccdd316c1
Exotica0122 <anpeteran2@gmail.com>
2024-04-01 13:13:32 +1300

    feat: add cursor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -49,8 +49,8 @@ const Home = () => {
   const [words, setWords] = useState("");
   const [wordsArray, setWordsArray] = useState<string[]>([]);
   const [wordsObject, setWordsObject] = useState<RandomWord[]>([]);
-  const [wordsError, setWordsError] = useState<string[]>([]);
   const [inputFocus, setInputFocus] = useState(false);
+  const [disableInput, setDisableInput] = useState(false);
 
   const [value, setValue] = useState("");
   const [started, setStarted] = useState(false);
@@ -59,6 +59,16 @@ const Home = () => {
   const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
   const [backspaceMinPosition, setBackspaceMinPosition] = useState(0);
+
+  console.log(currentLetterIndex);
+
+  const cursorRef = useRef<HTMLDivElement | null>(null);
+  const [cursorElementPosition, setCursorElementPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  let wordsRef = useRef<HTMLDivElement[]>([]);
 
   useEffect(() => {
     inputRef.current?.focus();
@@ -70,7 +80,81 @@ const Home = () => {
     setWordsObject(randomWordsObject);
   }, []);
 
+  useEffect(() => {
+    if (started) {
+      handleCursor();
+    }
+  }, [currentWordIndex, currentLetterIndex]);
+
+  function handleCursor() {
+    if (!cursorRef || !cursorRef.current) {
+      return;
+    }
+
+    const cursorPositionX = cursorRef.current.getBoundingClientRect().left;
+    const cursorPositionY = cursorRef.current.getBoundingClientRect().top;
+
+    let newCursorPositionX = 0;
+    let newCursorPositionY = 0;
+
+    if (currentLetterIndex === 0) {
+      newCursorPositionX =
+        wordsRef.current[currentWordIndex].childNodes[
+          currentLetterIndex
+        ].getBoundingClientRect().left;
+      newCursorPositionY =
+        wordsRef.current[currentWordIndex].childNodes[
+          currentLetterIndex
+        ].getBoundingClientRect().top;
+    } else {
+      newCursorPositionX =
+        wordsRef.current[currentWordIndex].childNodes[
+          currentLetterIndex - 1
+        ].getBoundingClientRect().right;
+      newCursorPositionY =
+        wordsRef.current[currentWordIndex].childNodes[
+          currentLetterIndex - 1
+        ].getBoundingClientRect().top;
+    }
+
+    const xOffset = newCursorPositionX - cursorPositionX;
+    const yOffset = newCursorPositionY - cursorPositionY;
+    setCursorElementPosition((prevPosition) => ({
+      x: (prevPosition.x += xOffset),
+      // y: prevPosition.y,
+      y: (prevPosition.y += yOffset),
+    }));
+  }
+
+  const handleEnableTyping = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (
+      e.code === "MetaLeft" ||
+      e.code === "MetaRight" ||
+      e.code === "AltLeft" ||
+      e.code === "ControlLeft"
+    ) {
+      setDisableInput(false);
+      return;
+    }
+  };
+
   const handleTyping = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (
+      e.code === "MetaLeft" ||
+      e.code === "MetaRight" ||
+      e.code === "AltLeft" ||
+      e.code === "ControlLeft"
+    ) {
+      setDisableInput(true);
+      return;
+    }
+
+    if (!started) {
+      setStarted(true);
+    }
+
+    if (disableInput) return;
+
     if (e.code === "Backspace") {
       console.log(wordsObject[currentWordIndex - 1]);
       if (
@@ -138,6 +222,7 @@ const Home = () => {
     }
 
     if (value.length <= 20) {
+      // console.log(currentLetterIndex);
       setValue((prevValue) => prevValue + e.key);
       setCurrentLetterIndex((prevIndex) => prevIndex + 1);
     }
@@ -169,9 +254,16 @@ const Home = () => {
   };
 
   return (
-    <>
+    <div className="w-[80%]">
       <div
-        className="w-[80%] flex flex-wrap gap-2 text-2xl select-none"
+        className={`animate-blink absolute mt-1 h-6 w-[2px] bg-yellow-300`}
+        style={{
+          transform: `translate(${cursorElementPosition.x}px, ${cursorElementPosition.y}px)`,
+        }}
+        ref={cursorRef}
+      />
+      <div
+        className="flex flex-wrap gap-2 text-2xl select-none"
         onClick={() => inputRef.current?.focus()}
       >
         {wordsObject.map((wordObject, i) => {
@@ -180,6 +272,7 @@ const Home = () => {
               className={`w-fit${
                 wordObject.isError ? " underline decoration-red-500" : ""
               }`}
+              ref={(el) => (wordsRef.current[i] = el)}
             >
               {wordObject.word.map(({ id, value, isError, isTyped }) => {
                 let letterClassName = "";
@@ -190,7 +283,9 @@ const Home = () => {
                 } else {
                   letterClassName = "text-neutral-400";
                 }
-                return <span className={`${letterClassName}`}>{value}</span>;
+                return (
+                  <span className={`${letterClassName} ml-[2px]`}>{value}</span>
+                );
               })}
             </div>
           );
@@ -200,17 +295,17 @@ const Home = () => {
       <input
         ref={inputRef}
         className="opacity-0 p-0 m-0 absolute select-none"
-        // className="p-0 m-0 absolute select-none text-black"
         value={value}
         onFocus={() => setInputFocus(true)}
         onBlur={() => setInputFocus(false)}
         onKeyDown={handleTyping}
+        onKeyUp={handleEnableTyping}
         autoComplete="false"
         autoCapitalize="false"
         data-enable-grammarly="false"
         spellCheck="false"
       />
-    </>
+    </div>
   );
 };
 
