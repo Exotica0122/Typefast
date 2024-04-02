c27935e2269ecbded71ab2dcfde7ba3f1f7574a0
Exotica0122 <anpeteran2@gmail.com>
2024-04-02 22:46:41 +1300

    style: add cursor to focus window

--- src/components/FocusWindow.tsx
+++ src/components/FocusWindow.tsx
@@ -1,3 +1,5 @@
+import { PiCursorFill } from "react-icons/pi";
+
 type FocusWindowProps = {
   showBlur: boolean;
 };
@@ -5,8 +7,9 @@ type FocusWindowProps = {
 export const FocusWindow = ({ showBlur }: FocusWindowProps) => {
   return (
     <div
-      className={`absolute flex h-full w-full items-center justify-center text-center transition-opacity duration-500 ${showBlur ? "opacity-100" : "opacity-0"}`}
+      className={`absolute flex h-full w-full items-center justify-center gap-2 text-center transition-opacity duration-500 ${showBlur ? "opacity-100" : "opacity-0"}`}
     >
+      <PiCursorFill />
       Click here to focus
     </div>
   );
