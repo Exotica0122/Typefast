37356ce06fa6bda125b7046980cb3dc7ce43fa69
Peter An <75343007+Exotica0122@users.noreply.github.com>
2024-04-04 20:47:51 +1300

    feat: add vercel analytics (#8)

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
     <Toaster position="bottom-center" />
+    <Analytics />
   </React.StrictMode>,
 );
