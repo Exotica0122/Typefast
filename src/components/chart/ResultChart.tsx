38fda4a4dc28aaa6f75e1d3ede4bd940a2e80a1f
Peter An <75343007+Exotica0122@users.noreply.github.com>
2024-04-05 13:43:48 +1300

    chore: rename getResultChartOptions

--- src/components/chart/ResultChart.tsx
+++ src/components/chart/ResultChart.tsx
@@ -9,7 +9,7 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
-import { getResultChartOptions } from "./resultChartOptions";
+import { getResultChartOptions } from "./getResultChartOptions";
 
 ChartJS.register(
   CategoryScale,
