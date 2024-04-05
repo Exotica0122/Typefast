3b62fa0bd1484d3c12f54e444e36a4a7e577e2e5
Peter An <75343007+Exotica0122@users.noreply.github.com>
2024-04-05 18:21:04 +1300

    feat: add typing setting

---
+++ src/components/TypingSetting.tsx
@@ -0,0 +1,11 @@
+export const TypingSetting = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <ul className="flex items-center justify-center gap-6 rounded-md bg-neutral-900 py-2 px-6">
+        <li>15</li>
+        <li>30</li>
+        <li>60</li>
+      </ul>
+    </div>
+  );
+};
